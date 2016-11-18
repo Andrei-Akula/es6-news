a4f3845ae8a13c265f6f79faabb977e68c065f44
aakula <aakula@edmunds.com>
2016-11-19 00:00:32 +0300

    webpack config

---
+++ res/webpack/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var cwd = process.cwd();
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(cwd, 'dist/'),
+        filename: "bundle.js",
+        publicPath: "/assets/"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.html', '.css'],
+        modules: [cwd, 'node_modules']
+    },
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-function getJSON(url) {
-    'use strict';
-    var xhr = new XMLHttpRequest();
-    return new Promise((resolve, reject) => {
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject(xhr.responseText);
-                }
-            }
-        };
-        xhr.open('GET', url);
-        xhr.send();
-    });
-}
+var ajax = require('./ajax/helpers');
