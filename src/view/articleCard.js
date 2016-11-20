5fb58ee77f76641e45381a54d1719356f54d4314
aakula <aakula@edmunds.com>
2016-11-20 23:56:03 +0300

    First release

---
+++ src/view/articleCard.js
@@ -0,0 +1,14 @@
+function render(article) {
+    return `<div class="card">
+  <img class="card-img-top w-100" src="${article.urlToImage}" alt="${article.title}">
+  <div class="card-block">
+    <h4 class="card-title">${article.title}</h4>
+    <p class="card-text">${article.description}</p>
+    <a href="${article.url}" target="_blank" class="btn btn-primary">Read on BBC</a>
+  </div>
+</div>`;
+}
+
+module.exports = {
+    render
+};
--- src/view/render.js
+++ src/view/render.js
@@ -1,9 +1,20 @@
-function displayNews(elemId, news) {
+require('./view.css');
+let articleCard = require('./articleCard');
 
+function displayNews(elemId, news) {
+    let elem = document.getElementById(elemId);
+    if (elem && news.status == 'ok') {
+        elem.innerHTML = news.articles
+            .map(a => '<li class="col-xs-12 col-md-8">' + articleCard.render(a) + '</li>')
+            .reduce((txt, li) => txt + li, '<ul class="articles row">') + '</ul>';
+    }
 }
 
 function displyError(elemId, reason) {
-
+    let elem = document.getElementById(elemId);
+    if (elem) {
+        elem.innerHTML = reason;
+    }
 }
 
 module.exports = {
