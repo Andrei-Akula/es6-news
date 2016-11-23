10afadf3a7583b177a74ab632934133ea24c07c0
aakula <aakula@edmunds.com>
2016-11-23 10:06:05 +0300

    use Request and fetch instead of XMLHttpRequest

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ let newsSrc = 'bbc-news',
     newsApiKey = '7075477dd0074880bc9b96eafd22cd03',
     containerId = 'main';
 
+let url = `https://newsapi.org/v1/articles?source=${newsSrc}&apiKey=${newsApiKey}`;
 
-getJSON(`https://newsapi.org/v1/articles?source=${newsSrc}&apiKey=${newsApiKey}`)
-    .then(news => { renderNews(containerId, news) })
+// getJSON(url)
+//     .then(news => { renderNews(containerId, news) })
+//     .catch(reason => { renderError(containerId, reason) });
+
+
+fetch(new Request(url))
+    .then(response =>  {
+        response.json().then(news => { renderNews(containerId, news) } );
+    })
     .catch(reason => { renderError(containerId, reason) });
